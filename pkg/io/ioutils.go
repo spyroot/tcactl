e42528a069ba33d6a881332580c0ba629020c251
spyroot <spyroot@gmail.com>
2021-06-22 17:24:47 +0400

    CSAR zipper tool

---
+++ pkg/io/ioutils.go
@@ -0,0 +1,135 @@
+// Package app
+// Copyright 2020-2021 Author.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// Mustafa mbayramo@vmware.com
+
+package io
+
+import (
+	"bytes"
+	"compress/gzip"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+/**
+
+ */
+func bindataRead(data []byte, name string) ([]byte, error) {
+	gz, err := gzip.NewReader(bytes.NewBuffer(data))
+	if err != nil {
+		return nil, fmt.Errorf("read %q: %v", name, err)
+	}
+
+	var buf bytes.Buffer
+	_, err = io.Copy(&buf, gz)
+	clErr := gz.Close()
+
+	if err != nil {
+		return nil, fmt.Errorf("read %q: %v", name, err)
+	}
+	if clErr != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// CheckSocket
+// Return if given IP:PORT bind open or not, network must
+// must be "tcp", "tcp4", "tcp6", "unix" or "unixpacket".
+// No validation, caller must do that/*
+func CheckSocket(hostPort string, proto string) bool {
+
+	l, err := net.Listen(proto, hostPort)
+	if l != nil {
+		defer l.Close()
+	}
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+// ReadUint64 /**
+func ReadUint64(data []byte) (ret uint64) {
+	buf := bytes.NewBuffer(data)
+	binary.Read(buf, binary.LittleEndian, &ret)
+	return
+}
+
+// IsDir /**
+func IsDir(dir string) (bool, error) {
+
+	if len(dir) == 0 {
+		return false, nil
+	}
+
+	src, err := os.Stat(dir)
+	if err != nil {
+		return false, err
+	}
+
+	if !src.IsDir() {
+		return false, err
+	}
+	return true, nil
+}
+
+// FileExists /*
+func FileExists(filename string) bool {
+	stat, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !stat.IsDir()
+}
+
+func _filePath(dir, name string) string {
+	canonicalName := strings.Replace(name, "\\", "/", -1)
+	return filepath.Join(append([]string{dir}, strings.Split(canonicalName, "/")...)...)
+}
+
+// PrettyPrint pretty printer
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		fmt.Println(string(b))
+	}
+	return
+}
+
+// PrettyString print string as pretty json
+func PrettyString(v interface{}) string {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err == nil {
+		return string(b)
+	}
+	return ""
+}
+
+func CheckErr(msg interface{}) {
+	if msg != nil {
+		fmt.Fprintln(os.Stderr, "Error:", msg)
+		os.Exit(1)
+	}
+}
---
+++ pkg/io/zipper.go
@@ -0,0 +1,175 @@
+// Package io
+// Copyright 2020-2021 Author.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// Mustafa mbayramo@vmware.com
+
+package io
+
+import (
+	"archive/zip"
+	"fmt"
+	"github.com/golang/glog"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func test() {
+	files, err := Unzip("test.zip", "uncompressed_dir")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Unzipped the following files:\n" + strings.Join(files, "\n"))
+}
+
+// Unzip decompress a zip archived file
+// copying all files and folders
+// within the zip file to an output
+// destination directory
+func Unzip(src string, destination string) ([]string, error) {
+
+	var filenames []string
+	r, err := zip.OpenReader(src)
+
+	if err != nil {
+		return filenames, err
+	}
+
+	defer r.Close()
+
+	for _, f := range r.File {
+		fpath := filepath.Join(destination, f.Name)
+
+		// Checking for any invalid file paths
+		if !strings.HasPrefix(fpath, filepath.Clean(destination)+string(os.PathSeparator)) {
+			return filenames, fmt.Errorf("%s is an illegal filepath", fpath)
+		}
+
+		filenames = append(filenames, fpath)
+		if f.FileInfo().IsDir() {
+			// Creating a new Folder
+			err := os.MkdirAll(fpath, os.ModePerm)
+			if err != nil {
+				return nil, err
+			}
+			continue
+		}
+
+		if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+			return filenames, err
+		}
+
+		// The created file will be stored in
+		// outFile with permissions to write &/or truncate
+		outFile, err := os.OpenFile(fpath,
+			os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+			f.Mode())
+
+		if err != nil {
+			return filenames, err
+		}
+
+		rc, err := f.Open()
+		if err != nil {
+			return filenames, err
+		}
+
+		_, err = io.Copy(outFile, rc)
+
+		outFile.Close()
+		rc.Close()
+
+		if err != nil {
+			return filenames, err
+		}
+	}
+	return filenames, nil
+}
+
+// ZipDir zip source dir and writes
+// to target zip file
+func ZipDir(source, target string) error {
+
+	glog.Infof("Using base dir %v", source)
+
+	zipFile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipFile.Close()
+
+	archive := zip.NewWriter(zipFile)
+	defer archive.Close()
+
+	info, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	var baseDir string
+	if info.IsDir() {
+		glog.Infof("Using base dir %v", baseDir)
+		baseDir = filepath.Base(source)
+	}
+
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		if baseDir != "" {
+			// adjust path to everything compress relative to 'base'
+			header.Name = filepath.Join("", strings.TrimPrefix(path, source))
+		}
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	return err
+}
