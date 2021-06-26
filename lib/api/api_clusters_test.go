5b66afa651f60d0fed4257d439587e8ea20e0f2a
spyroot <spyroot@gmail.com>
2021-06-26 22:44:34 +0400

    Added capability create or remove node pool

--- lib/api/api_clusters_test.go
+++ lib/api/api_clusters_test.go
@@ -1,3 +1,20 @@
+// Package api
+// Copyright 2020-2021 Author.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+// Mustafa mbayramo@vmware.com
 package api
 
 import (
@@ -74,7 +91,7 @@ func TestTcaApi_GetClusterTask(t *testing.T) {
 				return
 			}
 
-			clusterTask, err := a.GetClusterTask(getTestClusterName())
+			clusterTask, err := a.GetClusterTask(getTestClusterName(), true)
 			if err != nil {
 				return
 			}
